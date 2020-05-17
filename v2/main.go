e24bea4bd3d4c565ed30b7456a2f92b02dcd2dd2
wangyouhua <wang.youhua@tvsee.cn>
2020-05-18 07:58:39 +0800

    modify Add and Mul function to variadic function

---
+++ v2/main.go
@@ -0,0 +1,48 @@
+package go_math
+
+import (
+	"errors"
+)
+
+func Add(args ...int) int {
+	result :=0
+	for _, v := range args{
+		result +=v
+	}
+	return result
+}
+
+func Sub(a, b int) int {
+	return a-b
+}
+
+func Mul(args  ...int) int {
+	result :=0
+	for _, v := range args{
+		result *=v
+	}
+	return result
+}
+
+func Div(a, b int) (int, error) {
+	if 0 == b {
+		return 0, errors.New("Not allow divided by zero")
+	}
+	return a/b, nil
+}
+
+func Power(a, b int) int {
+	if 1 == b {
+		return  a
+	}
+
+	if 0 == b {
+		return 1
+	}
+
+	if 0 == b&1  {
+		half := Power(a, b/2)
+		return half*half
+	}
+	return Power(a, b-1) * a
+}
